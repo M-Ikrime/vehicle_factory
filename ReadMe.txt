a8153e494ad4a6a930acbdaaf9a4b195d2e6634a
M-Ikrime <myagbasa120@gmail.com>
2023-05-01 13:37:26 +0300

    Proje dosyası ekle.

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+Eren Çiçek - 211216070
+Adem Mert Bıyık - 211216050
+Muhammed İkrime Yağbasa - 211216040
+Abdullah İnce - 211216014
+
+Projemizi Code-First yaklaşımıyla yaptık. Entity Framework 6.0 teknolojisini kullanarak
+database ile entegrasyonunu yaptık.
---
+++ UretimTakip/Business/Repositories/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UretimTakip.Business
+{
+    public interface IGenericService<T> where T : class
+    {
+        public void Add(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
+        public IQueryable<T> GetAll();
+        public T GetById(int id);
+        public T GetBy(Expression<Func<T, bool>> expression);
+    }
+}
